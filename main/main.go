e41836f49862bb0e68f5e74cd13324a7e071670a
Timothy C. Lam <geroginalam@hotmail.com>
2019-10-13 00:10:01 +0800

    Initial commit

---
+++ main/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	
+	urlShortener "github.com/ttimt/urlshort-gophercises"
+)
+
+func main() {
+	mux := defaultMux()
+	
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlShortener-godoc": "https://godoc.org/github.com/gophercises/urlShortener",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := urlShortener.MapHandler(pathsToUrls, mux)
+	
+	// Build the YAMLHandler using the mapHandler as the
+	// fallback
+	yaml := `
+- path: /urlShortener
+  url: https://github.com/gophercises/urlShortener
+- path: /urlShortener-final
+  url: https://github.com/gophercises/urlShortener/tree/solution
+`
+	yamlHandler, err := urlShortener.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on :8080")
+	_ = http.ListenAndServe(":8080", yamlHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintln(w, "Hello, world!")
+}
